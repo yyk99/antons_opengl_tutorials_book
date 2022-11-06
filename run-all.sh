c162a2aa652de757938e7312cc713e4f64afd0c9
Yuri Y. Kuznetsov <yyk@kestrel>
2022-11-06 08:40:10 -0500

    Updated 	modified:   27_font_atlas/CMakeLists.txt 	modified:   run-all.sh

--- run-all.sh
+++ run-all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 for i in $(/bin/find ??_* -name \*.exe) ; do
-	echo $i
+	# echo $i
 	exe=$(realpath $i)
 	workdir=$(dirname $i)
 	(
 		cd $workdir
+		echo '>>>>>>> ' $workdir
 		$exe 
 	)
 done
